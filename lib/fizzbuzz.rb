cf111306cb4031f43128f4fcdcbd5d90c3f0619c
Jamie LeMercier <jamielemercier@gmail.com>
2017-10-18 15:43:22 +0100

    Refactors ternary operator for easy readibility

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,6 @@
 def fizzbuzz(number)
-  number % 3 == 0 && number % 5 == 0 ? 'fizzbuzz' : (number % 3 == 0 ? 'fizz' : (number % 5 == 0 ? 'buzz' : (number)))
+  number % 3 == 0 && number % 5 == 0 ? 'fizzbuzz'
+  : number % 3 == 0 ? 'fizz'
+    : number % 5 == 0 ? 'buzz'
+      : number
 end
