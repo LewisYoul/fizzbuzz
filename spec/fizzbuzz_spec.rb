c9c14b11bf46324b6fe2085c5592c39e80bfe666
Jamie LeMercier <jamielemercier@gmail.com>
2017-10-18 13:39:21 +0100

    Adds tests where number 5 and 10 should return buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 9' do
     expect(fizzbuzz(9)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
 end
